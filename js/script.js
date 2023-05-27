b92421f00c00d8fe50dfde5606218d677ea9531e
keita280 <tanakei1115@gmail.com>
2023-05-27 11:10:37 +0900

    一旦メインページ完成

--- js/script.js
+++ js/script.js
@@ -2,21 +2,20 @@
 
 
 //@@@@@@@@@@@@@@@@@@@@@@@@@
-//ハンバーガーメニューの表示
+// ハンバーガーメニューの開閉
 //@@@@@@@@@@@@@@@@@@@@@@@@@
 
-jQuery(function() {
-  $('#toggle').on('click', function() {
-    $('body').toggleClass('is-open');
-    if ($('#humberger-text').text() === 'CLOSE') {
-      $('#humberger-text').text('MENU');
-    } else {
-      $('#humberger-text').text('CLOSE');
-    }
+
+
+$(document).ready(function() {
+  $('.header__toggle').on('click', function() {
+    $(this).toggleClass('active');
+    $('.header__nav').toggleClass('active');
+    $('.sp-header__nav').toggleClass('is-active');
+
+
   });
 });
-
-
 
 
 //@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -46,28 +45,64 @@ jQuery('a[href^="#"]').click(function(){
 //@@@@@@@@@@@@@@@@@@@@@@@@@
 //モーダルウィンドウ
 //@@@@@@@@@@@@@@@@@@@@@@@@@
- $(function(){
-  // 変数に要素を入れる
-  var open = $('.modal-open'),
-    close = $('.modal-close'),
-    container = $('.modal-container');
+//  $(function(){
+//   // 変数に要素を入れる
+//   var open = $('.modal-open'),
+//     close = $('.modal-close'),
+//     container = $('.modal-container');
 
-  //開くボタンをクリックしたらモーダルを表示する
-  open.on('click',function(){ 
-    container.addClass('active');
-    return false;
-  });
+//   //開くボタンをクリックしたらモーダルを表示する
+//   open.on('click',function(){ 
+//     container.addClass('active');
+//     return false;
+//   });
 
-  //閉じるボタンをクリックしたらモーダルを閉じる
-  close.on('click',function(){  
-    container.removeClass('active');
-  });
+//   //閉じるボタンをクリックしたらモーダルを閉じる
+//   close.on('click',function(){  
+//     container.removeClass('active');
+//   });
 
-  //モーダルの外側をクリックしたらモーダルを閉じる
-  $(document).on('click',function(e) {
-    if(!$(e.target).closest('.modal-body').length) {
-      container.removeClass('active');
+//   //モーダルの外側をクリックしたらモーダルを閉じる
+//   $(document).on('click',function(e) {
+//     if(!$(e.target).closest('.modal-body').length) {
+//       container.removeClass('active');
+//     }
+//   });
+// });
+
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@
+  // YouTubeの埋め込みプレーヤーの幅を画面幅最大に調整する関数
+//@@@@@@@@@@@@@@@@@@@@@@@@@
+$(function(){
+  function adjustYouTubePlayerSize() {
+    var container = document.querySelector('.video-container');
+    var player = document.querySelector('#youtube-player iframe');
+
+    var containerWidth = container.offsetWidth;
+    var playerWidth = containerWidth;
+
+    // 画面幅が最大幅（例: 980px）より大きい場合のみ最大幅を設定
+    var maxWidth = 980;
+    if (containerWidth > maxWidth) {
+      playerWidth = maxWidth;
     }
-  });
-});
 
+    var playerHeight = (playerWidth / 16) * 9; // YouTubeプレーヤーのアスペクト比は16:9
+
+    player.style.width = playerWidth + 'px';
+    player.style.height = playerHeight + 'px';
+  }
+
+  // ウィンドウのリサイズ時にYouTubeプレーヤーのサイズを調整する
+  window.addEventListener('resize', adjustYouTubePlayerSize);
+
+  // ページ読み込み時にYouTubeプレーヤーのサイズを初期設定する
+  window.addEventListener('DOMContentLoaded', adjustYouTubePlayerSize);
+
+
+
+
+
+
+});
